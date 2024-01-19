9d1238edc9f8e1ea016069bf47ee489bc5baebed
aHasnat1997 <hasnat56703@gmail.com>
2024-01-20 01:16:53 +0600

    ADD: Log In page

--- src/routes/mainRouter.tsx
+++ src/routes/mainRouter.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import LogIn from "../pages/auth/LogIn";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
+    },
+    {
+        path: '/log-in',
+        element: <LogIn />
     }
 ]);
 
